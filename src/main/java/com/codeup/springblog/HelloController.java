1161d5cb0bfb261d7ef075df0777dd46fa900a7f
Laura Davis <laura.l.davis.210@gmail.com>
2020-07-31 11:28:13 -0500

    Controllers lecture and Devtools setup

---
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -0,0 +1,43 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello from Spring!";
+    }
+
+    @GetMapping("/goodbye")
+    @ResponseBody
+    public String goodbye(){
+        return "Goodbye from Spring!";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name){
+        return "Hello " + name +" from a GetMapping path!";
+    }
+
+    @GetMapping("/books/{id}")
+    @ResponseBody
+    public String getBook(@PathVariable long id){
+        // Connect to db to get book for this id
+        // Return view
+        return "Viewing book " + id;
+    }
+
+//    @PostMapping - will do this later
+
+    @RequestMapping(path = "/hello2/{myName}", method = RequestMethod.GET)
+    @ResponseBody
+    public String sayHello2(@PathVariable String myName){
+        return "Hello " + myName +" from a RequestMapping path!";
+    }
+
+}
