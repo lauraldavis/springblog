482e4a32282917ee6cf7f81150048729be8a3169
Laura Davis <laura.l.davis.210@gmail.com>
2020-08-04 20:55:14 -0500

    Repositories & JPA exercise - edit/delete Posts

---
+++ springblog_seeder.sql
@@ -0,0 +1,30 @@
+USE springblog;
+
+TRUNCATE TABLE posts;
+
+INSERT INTO posts (id, title, body) VALUES
+(1,'African Liverseed Grass','Poaceae/Urochloa mosambicensis (Hack.) Dandy'),
+(2,'Vanillaleaf','Asteraceae/Carphephorus odoratissimus (J.F. Gmel.) Herb.'),
+(3,'Western Catchfly','Caryophyllaceae/Silene occidentalis S. Watson ssp. longistipitata C.L. Hitchc. & Maguire'),
+(4,'Rayless Shaggy Fleabane','Asteraceae/Erigeron aphanactis (A. Gray) Greene var. aphanactis'),
+(5,'Streambank Rabbitsfoot Grass','Poaceae/Polypogon elongatus Kunth'),
+(6,'Tall Poppymallow','Malvaceae/Callirhoe leiocarpa R.F. Martin'),
+(7,'Northern Lecidea Lichen','Lecideaceae/Lecidea polaris Lynge'),
+(8,'Cupped Monolopia','Asteraceae/Monolopia major DC.'),
+(9,'Hairy Goldenrod','Asteraceae/Solidago hispida Muhl. ex Willd. var. hispida'),
+(10,'Stahl\'s Stopper','Myrtaceae/Eugenia stahlii (Kiaersk.) Krug & Urb.'),
+(11,'Alabama Supplejack','Rhamnaceae/Berchemia scandens (Hill) K. Koch'),
+(12,'Yellow Sunnybell','Liliaceae/Schoenolirion croceum (Michx.) Alph. Wood'),
+(13,'Sitka Willow','Salicaceae/Salix sitchensis Sanson ex Bong.'),
+(14,'Nettleleaf Giant Hyssop','Lamiaceae/Agastache urticifolia (Benth.) Kuntze'),
+(15,'Zornia','Fabaceae/Zornia J.F. Gmel.'),
+(16,'Acrocordia Lichen','Monoblastiaceae/Acrocordia gemmata (Ach.) A. Massal.'),
+(17,'Mountain Tail-leaf','Asteraceae/Pericome caudata A. Gray'),
+(18,'Waterlily','Nymphaeaceae/Nymphaea ×thiona Ward'),
+(19,'Lyall\'s Beardtongue','Scrophulariaceae/Penstemon lyallii (A. Gray) A. Gray'),
+(20,'Douglas\' Spineflower','Polygonaceae/Chorizanthe douglasii Benth.'),
+(21,'Sticky Blue Eyed Mary','Scrophulariaceae/Collinsia rattanii A. Gray ssp. rattanii'),
+(22,'Phyllostegia','Lamiaceae/Phyllostegia ×yamaguchii Hosaka & O. Deg. (pro sp.)'),
+(23,'Climbing Cactus','Cactaceae/Epiphyllum hookeri (Link & Otto) Haw.'),
+(24,'Swampforest Clermontia','Campanulaceae/Clermontia waimeae Rock'),
+(25,'Berteron\'s Sandmat','Euphorbiaceae/Chamaesyce berteriana (Balbis ex Spreng.) Millsp.');
--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -1,9 +1,22 @@
 package com.codeup.springblog.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT(11) UNSIGNED")
     private long id;
+
+    @Column(length = 100, nullable = false, unique = true)
     private String title;
+
+    @Column(length = 255, nullable = false)
     private String body;
+
+    public Post(){}
 
     public Post(long id, String title, String body) {
         this.id = id;
@@ -22,5 +35,10 @@ public class Post {
     public String getBody() { return body; }
 
     public void setBody(String body) { this.body = body; }
+
+    @Override
+    public String toString() {
+        return "Post{id=" + id + ",title='" + title + "', body='" + body + "'}";
+    }
 
 }
